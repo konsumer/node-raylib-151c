f83845dd464a7b22cc93b3f9b6191bc67c1063b2
David Konsumer <konsumer@jetboystudio.com>
2022-05-31 19:23:15 -0700

    port js example to C

--- sources/main.c
+++ sources/main.c
@@ -1,30 +1,27 @@
 #include "raylib.h"
 
-#define SCREEN_WIDTH (800)
-#define SCREEN_HEIGHT (450)
-
-#define WINDOW_TITLE "Window title"
-
 int main(void)
 {
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
+    InitWindow(300, 300, "Image Draw Test");
     SetTargetFPS(60);
 
-    Texture2D texture = LoadTexture(ASSETS_PATH"test.png"); // Check README.md for how this works
+    Image MapImage = GenImageColor(200, 200, RED);
+
+    Rectangle rect = {50,10,10,5};
+
+    ImageDrawPixel(&MapImage, 5, 5, YELLOW);
+    ImageDrawRectangle(&MapImage, 10, 10, 10, 10, GREEN);
+    ImageDrawCircle(&MapImage, 50, 50, 10, PINK);
+    ImageDrawRectangleLines(&MapImage, rect, 1, BLUE);
+
+    Texture MapTexture = LoadTextureFromImage(MapImage);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
+        ClearBackground(BLACK);
 
-        ClearBackground(RAYWHITE);
-
-        const int texture_x = SCREEN_WIDTH / 2 - texture.width / 2;
-        const int texture_y = SCREEN_HEIGHT / 2 - texture.height / 2;
-        DrawTexture(texture, texture_x, texture_y, WHITE);
-
-        const char* text = "OMG! IT WORKS!";
-        const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
-        DrawText(text, SCREEN_WIDTH / 2 - text_size.x / 2, texture_y + texture.height + text_size.y + 10, 20, BLACK);
+        DrawTexture(MapTexture, 50, 50, WHITE);
 
         EndDrawing();
     }
